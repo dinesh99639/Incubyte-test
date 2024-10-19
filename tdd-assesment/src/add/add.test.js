0c54ee12e12f4d4275e7f9042fcc21fb238f4fef
Dinesh Somaraju <dinesh99639@gmail.com>
2024-10-19 18:04:11 +0530

    Wrap all the tests related to Add function in a describe block

--- tdd-assesment/src/add/add.test.js
+++ tdd-assesment/src/add/add.test.js
@@ -1,10 +1,12 @@
 const add = require("./add");
 
-test("Returns 0 if empty string is given", () => {
-  expect(add("")).toBe(0);
-});
+describe("Add function", () => {
+  test("Returns 0 if empty string is given", () => {
+    expect(add("")).toBe(0);
+  });
 
-test("Return sum if comma separated values are passed", () => {
-  expect(add("1")).toBe(1);
-  expect(add("1,5")).toBe(6);
+  test("Return sum if comma separated values are passed", () => {
+    expect(add("1")).toBe(1);
+    expect(add("1,5")).toBe(6);
+  });
 });
