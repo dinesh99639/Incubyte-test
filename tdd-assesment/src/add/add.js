1c7891f07201aa1a1e169f0133976fd2a1d518c2
Dinesh Somaraju <dinesh99639@gmail.com>
2024-10-19 18:47:33 +0530

    Move getting delimiter logic to getDelimiter function

--- tdd-assesment/src/add/add.js
+++ tdd-assesment/src/add/add.js
@@ -1,9 +1,4 @@
-const add = (numbers) => {
-  if (numbers === "") {
-    return 0;
-  }
-
-  const negatives = [];
+const getDelimiter = (numbers) => {
   let delimiter = ",";
 
   if (numbers.slice(0, 2) === "//") {
@@ -12,6 +7,19 @@ const add = (numbers) => {
     delimiter = numbers.slice(2, index + 1).trim();
     numbers = numbers.slice(index + 1);
   }
+
+  return [delimiter, numbers];
+};
+
+const add = (numbers) => {
+  if (numbers === "") {
+    return 0;
+  }
+
+  const negatives = [];
+  let delimiter;
+
+  [delimiter, numbers] = getDelimiter(numbers);
 
   const array = numbers.replaceAll("\n", delimiter).split(delimiter);
   const res = array.reduce((total, numString) => {
